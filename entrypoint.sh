36c20333e184719fdf4753c51b15b012748a6041
Jason Antman <jason@jasonantman.com>
2018-12-22 09:28:41 -0500

    fix entrypoint for last commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 set -o errexit
-readonly LOG_FILE="/var/log/twilio-ppp"
-exec 1>>$LOG_FILE
-exec 2>&1
 
 echo "Setting modem device to /dev/${MODEM_DEV} in /etc/ppp/peers/twilio"
 sed -i "s/ttyUSB0/${MODEM_DEV}/" /etc/ppp/peers/twilio
