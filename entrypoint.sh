56a61230bf7632a49ed9cd550971374ea3a9e8ab
Jason Antman <jason@jasonantman.com>
2018-12-22 08:14:51 -0500

    log to /var/log/twilio-ppp for debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-echo "Setting modem device to /dev/${MODEM_DEV} in /etc/ppp/peers/twilio"
+echo "Setting modem device to /dev/${MODEM_DEV} in /etc/ppp/peers/twilio" >> /var/log/twilio-ppp
 sed -i "s/ttyUSB0/${MODEM_DEV}/" /etc/ppp/peers/twilio
-echo "Executing 'pon twilio'..."
+echo "Executing 'pon twilio'..." >> /var/log/twilio-ppp
 pon twilio
--- ip-up.sh
+++ ip-up.sh
@@ -5,22 +5,22 @@ PPP_SPEED="$3"
 PPP_LOCAL="$4"
 PPP_REMOTE="$5"
 
-echo "ppp ip-up script called for ${PPP_IFACE} (local=${PPP_LOCAL} remote=${PPP_REMOTE} DNS=${DNS1} ${DNS2})"
+echo "ppp ip-up script called for ${PPP_IFACE} (local=${PPP_LOCAL} remote=${PPP_REMOTE} DNS=${DNS1} ${DNS2})" >> /var/log/twilio-ppp
 
-echo "removing current nameserver from /etc/resolv.conf"
+echo "removing current nameserver from /etc/resolv.conf" >> /var/log/twilio-ppp
 sed -i '/^nameserver/ d' /etc/resolv.conf
 
-echo "adding $DNS1 and $DNS2 to /etc/resolv.conf"
+echo "adding $DNS1 and $DNS2 to /etc/resolv.conf" >> /var/log/twilio-ppp
 echo -e "nameserver $DNS1\nnameserver $DNS2" >> /etc/resolv.conf
 
 # replace current default route with via $PPP_REMOTE dev $PPP_IFACE
-echo "Removing default route..."
+echo "Removing default route..." >> /var/log/twilio-ppp
 ip route del default
-echo "Adding new default route via $PPP_REMOTE dev $PPP_IFACE"
+echo "Adding new default route via $PPP_REMOTE dev $PPP_IFACE" >> /var/log/twilio-ppp
 ip route add default via $PPP_REMOTE dev $PPP_IFACE
 
-echo "Verifying with: ping -I $PPP_IFACE -c 3 api.twilio.com"
+echo "Verifying with: ping -I $PPP_IFACE -c 3 api.twilio.com" >> /var/log/twilio-ppp
 ping -I $PPP_IFACE -c 3 api.twilio.com
 
-echo "Running tinyproxy..." >> /dev/tty/0
+echo "Running tinyproxy..." >> /var/log/twilio-ppp
 /etc/init.d/tinyproxy start
