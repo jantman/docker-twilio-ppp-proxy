0c36d54ec14f185beb4df807f9a89ad115289bf8
Jason Antman <jason@jasonantman.com>
2018-12-22 00:14:11 -0500

    add version

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+gitver=$(git rev-parse --short HEAD)
+[ -z "$(git status --porcelain)" ] || gitver=${gitver}-dirty
+gitrmt=$(git config remote.origin.url)
+docker build \
+    --build-arg "git_version=${gitver}" \
+    --build-arg "git_remote=${gitrmt}"
+    -t jantman/twilio-ppp-proxy:${gitver} \
+    .
