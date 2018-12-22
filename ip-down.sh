a5ebf5f5b5f5c8cd94cf687f910103957c809283
Jason Antman <jason@jasonantman.com>
2018-12-22 00:37:43 -0500

    Revert "log to /dev/tty/0" - this doesn't work
    This reverts commit ea98d6be8cdfdd8c2eeeaf57da31daf19c21aa30.

--- ip-down.sh
+++ ip-down.sh
@@ -6,7 +6,7 @@ PPP_LOCAL="$4"
 PPP_REMOTE="$5"
 # also DNS1 and DNS2 are set
 
-echo "ppp ip-down script called for ${PPP_IFACE} (local=${PPP_LOCAL} remote=${PPP_REMOTE} DNS=${DNS1} ${DNS2})" >> /dev/tty/0
-echo "executing: pkill -f tinyproxy" >> /dev/tty/0
+echo "ppp ip-down script called for ${PPP_IFACE} (local=${PPP_LOCAL} remote=${PPP_REMOTE} DNS=${DNS1} ${DNS2})"
+echo "executing: pkill -f tinyproxy"
 # This **should** result in the container exiting...
 pkill -f tinyproxy
--- ip-up.sh
+++ ip-up.sh
@@ -5,24 +5,24 @@ PPP_SPEED="$3"
 PPP_LOCAL="$4"
 PPP_REMOTE="$5"
 
-echo "ppp ip-up script called for ${PPP_IFACE} (local=${PPP_LOCAL} remote=${PPP_REMOTE} DNS=${DNS1} ${DNS2})" >> /dev/tty/0
+echo "ppp ip-up script called for ${PPP_IFACE} (local=${PPP_LOCAL} remote=${PPP_REMOTE} DNS=${DNS1} ${DNS2})"
 
-echo "removing current nameserver from /etc/resolv.conf" >> /dev/tty/0
+echo "removing current nameserver from /etc/resolv.conf"
 sed -i '/^nameserver/ d' /etc/resolv.conf
 
-echo "adding $DNS1 and $DNS2 to /etc/resolv.conf" >> /dev/tty/0
+echo "adding $DNS1 and $DNS2 to /etc/resolv.conf"
 echo -e "nameserver $DNS1\nnameserver $DNS2" >> /etc/resolv.conf
 
 # replace current default route with via $PPP_REMOTE dev $PPP_IFACE
-echo "Removing default route..." >> /dev/tty/0
+echo "Removing default route..."
 ip route del default
-echo "Adding new default route via $PPP_REMOTE dev $PPP_IFACE" >> /dev/tty/0
+echo "Adding new default route via $PPP_REMOTE dev $PPP_IFACE"
 ip route add default via $PPP_REMOTE dev $PPP_IFACE
 
-echo "Verifying with: ping -I $PPP_IFACE -c 3 api.twilio.com" >> /dev/tty/0
+echo "Verifying with: ping -I $PPP_IFACE -c 3 api.twilio.com"
 ping -I $PPP_IFACE -c 3 api.twilio.com
 
-echo "Setting tinyproxy bind address..." >> /dev/tty/0
+echo "Setting tinyproxy bind address..."
 sed -i "s/#Bind 192.168.0.1/Bind $PPP_LOCAL/" /etc/tinyproxy/tinyproxy.conf
 
 echo "Running tinyproxy..." >> /dev/tty/0
