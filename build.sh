764163c7be19da1586601c6f7e7b00f6eff813c1
Jason Antman <jason@jasonantman.com>
2018-12-22 00:14:47 -0500

    typo

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/bash -e
 
 gitver=$(git rev-parse --short HEAD)
 [ -z "$(git status --porcelain)" ] || gitver=${gitver}-dirty
 gitrmt=$(git config remote.origin.url)
 docker build \
     --build-arg "git_version=${gitver}" \
-    --build-arg "git_remote=${gitrmt}"
+    --build-arg "git_remote=${gitrmt}" \
     -t jantman/twilio-ppp-proxy:${gitver} \
     .
+
+echo "Built image: jantman/twilio-ppp-proxy:${gitver}"
